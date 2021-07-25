4c907bba462f2eb92277b76d2235f8aaf4cc1e38
Davi Cunha Silva <davi.cunhasilva@doclerholding.com>
2021-07-25 12:57:55 +0200

    creates validation and fetcher

---
+++ src/fetch/index.ts
@@ -0,0 +1,49 @@
+import fetch from 'node-fetch';
+
+export async function fetcher(
+  endpoint: string,
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE',
+  body?: object
+): Promise<any> {
+  const response = await fetch(
+    endpoint,
+    {
+      method,
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body,
+    },
+  );
+
+  const data = await response.json();
+  return data;
+}
+
+export async function fetcherAuth(
+  endpoint: string,
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE',
+  authorization: string,
+  body?: object
+): Promise<any> {
+  const response = await fetch(
+    endpoint,
+    {
+      method,
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: authorization
+      },
+      body,
+    },
+  );
+
+  const data = await response.json();
+  return data;
+}
