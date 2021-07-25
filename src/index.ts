c246a48a1068ff182a23b8bc5c7f3d9ffcdd0f1f
Davi Cunha Silva <davi.cunhasilva@doclerholding.com>
2021-07-25 20:07:09 +0200

    clean up

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,6 @@
-import { decodeToken, generateToken } from './generators/jwt';
-
 export * from './checkers';
 export * from './fetch';
 export * from './generators';
 export * from './logger';
 export * from './sanitizer';
 export * from './validators';
-
-async function test() {
-  const encoded = await generateToken({ hello: 'world' }, '1d');
-
-  const decoded = await decodeToken(encoded);
-
-  console.log(decoded);
-}
-
-test();
