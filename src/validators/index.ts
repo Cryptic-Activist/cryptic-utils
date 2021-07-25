80e5381d5ee5630183e882c50beeb2f2c0c201e7
Davi Cunha Silva <davi.cunhasilva@doclerholding.com>
2021-07-25 15:03:33 +0200

    improves validator function~

--- src/validators/index.ts
+++ src/validators/index.ts
@@ -1,21 +1,27 @@
-export function validate(bodyRequest: object, expectedRequest: object): boolean {
+export function validate(bodyRequest: object, expectedRequest: object): string[] {
+  const errors: string[] = [];
   Object.keys(bodyRequest).forEach((key, index) => {
-    if (key !== Object.keys(expectedRequest)[index]) return false;
-    if (typeof bodyRequest[key] !== typeof expectedRequest[key]) return false;
+    if (key !== Object.keys(expectedRequest)[index]) {
+      errors.push(`Expected the object key: ${Object.keys(expectedRequest)[index]}, but it received: ${key}; Property number ${index}`);
+    }
+    if (typeof bodyRequest[key] !== typeof expectedRequest[key]) {
+      errors.push(`Expected the type of ${typeof expectedRequest[key]}, but it received ${typeof bodyRequest[key]}; Property number ${index}`);
+    }
 
     if (typeof bodyRequest[key] === 'string') {
-      if (bodyRequest[key].length === 0) return false;
-      if (bodyRequest[key] !== expectedRequest[key]) return false;
+      if (bodyRequest[key].length === 0) {
+        errors.push(`The property ${key} must be at least one character long; Property number ${index}`);
+      }
     }
 
     if (typeof bodyRequest[key] === 'number') {
       try {
         Number(bodyRequest[key]);
       } catch (err) {
-        return false;
+        errors.push(`The property ${key} is not a number; Property number ${index}`);
       }
     }
   });
 
-  return true;
+  return errors;
 }
